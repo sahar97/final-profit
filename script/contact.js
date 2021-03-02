b67a65a5895bf15f4e130e9652948cca48a8cff2
unknown <you@example.com>
2021-03-02 14:50:12 +0200

    --- Auto-Git Commit ---

--- script/contact.js
+++ script/contact.js
@@ -1,16 +1,24 @@
 const fields_validations = {
     'first_name' : /^[a-zA-Z ]{2,30}$/,
     'last_name' : /^[a-zA-Z ]{2,30}$/,
+     'comName' :/^[0-9a-zA-Z]+$/,
+     'job' :/^[0-9a-zA-Z]+$/,
+     'email' :/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/,
+     'numb' :/[0][0-9]{9}/,
+     'comWeb' : /https?:\/\/[^\s]/,
     }
 
 function validate (event){
     var element=event.target;
+var elementId =event.target.id;
 
+    if ((element.value.match(fields_validations.elementId))) {
 
-    if (!(element.value.match(fields_validations.first_name))) {
-alert("true")    }
+        elementId.classList.add('invalid')
+
+    }
 else {
-    alert ("false ")
+    elementId.classList.remove('invalid')
 }
 }
 
